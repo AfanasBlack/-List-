f044daeae7a9af1b6bd2ae64307ca45f7244771a
Alexandr Afanasev <116384401+AfanasBlack@users.noreply.github.com>
2022-11-29 17:07:02 +0300

    Добавление методов

--- src/Main.java
+++ src/Main.java
@@ -13,14 +13,10 @@ public class Main {
 
         System.out.println("Добро пожаловать в наш магазин!");
 
-
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Выберете номер операции или введите end: ");
-            for (String o : operations) {
-                System.out.println("\t" + (operations.indexOf(o) + 1) + ". " + o);
-            }
+            listOperation(operations);
             String line = scanner.nextLine();
 
           if ("end".equals(line)) {
@@ -29,62 +25,76 @@ public class Main {
 
           int numberOperation = Integer.parseInt(line);
           if (numberOperation == 1) {
-
-              System.out.println("Какую покупку хотите добавить? ");
-              String title = scanner.nextLine();
-              product.add(title);
-              System.out.println("Итого покупок в списке: " + product.size());
+              addProduct(product, scanner);
 
           } else if (numberOperation == 2) {
-
-              System.out.println("Список покупок: ");
-              for (String p : product) {
-                  System.out.println("\t" + (product.indexOf(p) + 1) + ". " + p);
-              }
-              System.out.println();
+              listProduct(product);
 
           } else if (numberOperation == 3) {
+              listProduct(product);
+              removeProduct(product, scanner);
 
-              System.out.println("Список покупок: ");
-              for (String p : product) {
-                  System.out.println("\t" + (product.indexOf(p) + 1) + ". " + p);
-              }
-              System.out.println();
-
-              System.out.println("Какой товар хотите удалить? Введите номер или название ");
-              String removedProduct = scanner.nextLine();
-
-              if ("1".equals(removedProduct) || "2".equals(removedProduct) || "3".equals(removedProduct)) {
-                  int numberProduct = Integer.parseInt(removedProduct) - 1;
-                  String name = product.get(numberProduct);
-                  product.remove(name);
-                  System.out.println("Покупка " + name + " удалена, список покупок: ");
-
-                  for (String p : product) {
-                      System.out.println("\t" + (product.indexOf(p) + 1) + ". " + p);
-                  }
-                  System.out.println();
-
-              } else {
-                  product.remove(removedProduct);
-                  System.out.println("Покупка " + removedProduct + " удалена, список покупок: ");
-                  for (String p : product) {
-                      System.out.println("\t" + (product.indexOf(p) + 1) + ". " + p);
-                  }
-                  System.out.println();
-              }
           } else if (numberOperation == 4) {
-              System.out.println("Введите текст для поиска: ");
-              String searchProduct = scanner.nextLine();
-              String queryLower = searchProduct.toLowerCase();
-              System.out.println("Найдено: ");
-              for (String p : product) {
-                  String itemLower = p.toLowerCase();
-                  if (itemLower.contains(queryLower)) {
-                      System.out.println((product.indexOf(p) + 1) + ". " + p);
-                  }
-              }
+              searchProduct(product, scanner);
           }
         }
+    }
+
+    private static void searchProduct(ArrayList<String> product, Scanner scanner) {
+        System.out.println("Введите текст для поиска: ");
+        String searchProduct = scanner.nextLine();
+        String queryLower = searchProduct.toLowerCase();
+        System.out.println("Найдено: ");
+        for (String p : product) {
+            String itemLower = p.toLowerCase();
+            if (itemLower.contains(queryLower)) {
+                System.out.println((product.indexOf(p) + 1) + ". " + p);
+            }
+        }
+    }
+
+    private static void removeProduct(ArrayList<String> product, Scanner scanner) {
+        System.out.println("Какой товар хотите удалить? Введите номер или название ");
+        String removedProduct = scanner.nextLine();
+        try {
+            int numberRemove = Integer.parseInt(removedProduct);
+            if (numberRemove > 0) {
+                int numberProduct = Integer.parseInt(removedProduct) - 1;
+                String name = product.get(numberProduct);
+                product.remove(name);
+                System.out.println("Покупка " + name + " удалена, список покупок: ");
+                listProduct(product);
+            } else {
+                product.remove(removedProduct);
+                System.out.println("Покупка " + removedProduct + " удалена, список покупок: ");
+                listProduct(product);
+            }
+        } catch (NumberFormatException e) {
+            product.remove(removedProduct);
+            System.out.println("Покупка " + removedProduct + " удалена, список покупок: ");
+            listProduct(product);
+        }
+    }
+
+    private static void addProduct(ArrayList<String> product, Scanner scanner) {
+        System.out.println("Какую покупку хотите добавить? ");
+        String title = scanner.nextLine();
+        product.add(title);
+        System.out.println("Итого покупок в списке: " + product.size());
+    }
+
+    private static void listOperation(List<String> operations) {
+        System.out.println("Выберете номер операции или введите end: ");
+        for (String o : operations) {
+            System.out.println("\t" + (operations.indexOf(o) + 1) + ". " + o);
+        }
+    }
+
+    private static void listProduct(ArrayList<String> product) {
+        System.out.println("Список покупок: ");
+        for (String p : product) {
+            System.out.println("\t" + (product.indexOf(p) + 1) + ". " + p);
+        }
+        System.out.println();
     }
 }
